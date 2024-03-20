aaaf59a36b8b3ef181dfdeaa2f754446b1011fb9
Arpit1546 <darpit22it@student.mes.ac.in>
2024-03-21 00:17:48 +0530

    a new commit

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection parameters
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "fithub";
+
+// Establishing a connection to the database
+$conn = mysqli_connect( $host, $username, $password, $database);
+
+// Check the connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
