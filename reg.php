d1a07408db50a5d436767cf302c2a9cf2788f630
Arpit1546 <118746170+Arpit1546@users.noreply.github.com>
2024-03-25 19:10:30 +0530

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>register on fithub</title>
+  <link rel="stylesheet" href="login.css">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+  <div class="wrapper">
+    <form action="">
+      <h1>Register</h1>
+      <div class="input-box">
+        <input type="text" placeholder="Username" name="user" required>
+        <i class='bx bxs-user'></i>
+      </div>
+
+      <div class="input-box">
+        <input type="text" placeholder="Email" name="email" required>
+        <i class='bx bx-envelope'></i>
+      </div>
+      
+      <div class="input-box">
+        <input type="password" placeholder="Password" name="pass" required>
+        <i class='bx bxs-lock-alt' ></i>
+      </div>
+      
+      <button type="submit" class="btn">Login</button>
+      <div class="register-link">
+        <p>Already have an account? <a href="login.html">Login now!</a></p>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
